db6c979a3e80ed5dda0c81e833ef0b57add0de4f
EnesT51 <99040669@mydavinci.nl>
2021-12-09 10:07:09 +0100

    updaded

--- papi-gelato.py
+++ papi-gelato.py
@@ -1,6 +1,10 @@
 vraag = ""
+smaak =''
 def begin():
     print('Welkom bij Papi Gelato je mag alle smaken kiezen zolang het maar vanille ijs is. ')
+
+def bedankt():
+    print("Bedankt en tot ziens!")
 
 def sorry():
     print("sorry dat snap ik niet")
@@ -10,41 +14,61 @@ def grote():
 
 def bestellen():
     vraag3 = input('wilt u nog meer bestellen Y/N:? ')
-    if vraag3 == 'y' or vraag3 == 'Y'.lower():
+    if vraag3 == 'y' or vraag3 == 'Y':
         bolletjes_stap1()
-    elif vraag3 == 'n' or vraag3 == 'N'.lower():
+    elif vraag3 == 'n' or vraag3 == 'N':
         bedankt()
         quit
     else:
         sorry()
         bestellen()
 
-def bedankt():
-    print("Bedankt en tot ziens!")
-
-def vraagherhalen():
+def hoorentje_bakje():
     global vraag
+    global smaak
     vraag2 = input('Wilt u deze {} bolletje(s) in A) een hoorntje of B) een bakje? '.format(vraag)).lower()
-    if vraag2 =='a':
+    if vraag2 =='a' or vraag2 =='A':
+        smaken()
         print('Hier is uw hoorntje met {} bolletje(s).'.format(vraag))
         bestellen()
-    elif vraag2 =='b':
+        
+    elif vraag2 =='b' or vraag2 == 'B':
+        smaken()
         print("Hier is uw bakje met {} bolletje(s).".format(vraag))
         bestellen()
+        
     else:
         sorry()
-        vraagherhalen()
+        hoorentje_bakje()
+
 def bolletjes_stap1():
     global vraag
     vraag = int (input("Hoeveel bolletjes wilt u?: "))
     if vraag >=1 and vraag <=3:
-        vraagherhalen()
+        hoorentje_bakje()
     elif vraag >=4 and vraag <=8:
+        smaken()
         print("Dan krijgt u van mij een bakje met {} bolletjes".format(vraag))
-
+        bestellen()
     else:
         grote()
         bolletjes_stap1()
+def smaken():
+    nummer = 0
+    for i in range(int(vraag)):
+        nummer+=1
+        smaak = input('Welke smaak wilt u voor bolletje nummer {}? A) Aardbei, C) Chocolade, M) Munt of V) Vanille? '.format(nummer))
+        if smaak == 'a' or smaak == 'A':
+            smaak = 'Aardbei'
+        elif smaak == 'c' or smaak == 'C':
+            smaak = 'Chocolade'
+        elif smaak == 'm' or smaak == 'M':
+            smaak = 'Mint'
+        elif smaak == 'v' or smaak == 'V':
+            smaak = 'Vanille'
+        else:
+            sorry()
+            smaken()
 #(----------------------------------funcions above---------------------------------------------------)
 begin()
 bolletjes_stap1()
