5e6cfc88050020577939958aeb723ff396772ddf
EnesT51 <99040669@mydavinci.nl>
2021-12-13 12:16:16 +0100

    updated

--- papi-gelato.py
+++ papi-gelato.py
@@ -1,5 +1,16 @@
 vraag = ''
 smaak = ''
+
+prijsbolletjes = 1.10
+prijsbakje = 0.75
+prijshorrentjes = 1.25
+
+aantalbak = 0
+aantalhoorn = 0
+totaalbollen = 0
+
+
+
 def begin():
     print('Welkom bij Papi Gelato.')
 
@@ -12,47 +23,73 @@ def sorry():
 def grote():
     print("Sorry, zulke grote bakken hebben we niet")
 
+def bon():
+    global totaalbollen
+    totaalbollen += vraag
+    totaal1 = totaalbollen * prijsbolletjes
+    totaal2 = aantalbak * prijsbakje
+    totaal3 = aantalhoorn * prijshorrentjes
+
+    totaal = (totaalbollen * prijsbolletjes + aantalbak * prijsbakje + aantalhoorn * prijshorrentjes)
+    print("------------[Papi Gelato]-------------")
+    print("bollentjes",totaalbollen,"x", format(totaal1,".2f"))
+    print("hoornje",aantalhoorn,"x" , totaal3)
+    print("bakje",aantalbak,"x" , totaal2)
+    print("------------------------------------------------")
+    print("totaal moet je" , format(totaal ,".2f") , "betalen")
+    
+
 def bestellen():
     vraag3 = input('wilt u nog meer bestellen Y/N:? ')
     if vraag3 == 'y' or vraag3 == 'Y':
         bolletjes_stap1()
     elif vraag3 == 'n' or vraag3 == 'N':
         bedankt()
-        quit
+        quit()
     else:
         sorry()
         bestellen()
 
-def hoorentje_bakje():
+def hoorntje_bakje():
     global vraag
     global smaak
+    global aantalbak
+    global aantalhoorn
     vraag2 = input('Wilt u deze {} bolletje(s) in A) een hoorntje of B) een bakje? '.format(vraag)).lower()
     if vraag2 =='a' or vraag2 =='A':
+        aantalhoorn +=1
         smaken()
         print('Hier is uw hoorntje met {} bolletje(s).'.format(vraag))
+        bon()
         bestellen()
 
     elif vraag2 =='b' or vraag2 == 'B':
+        aantalbak +=1
         smaken()
         print("Hier is uw bakje met {} bolletje(s).".format(vraag))
+        bon()
         bestellen()
         
     else:
         sorry()
-        hoorentje_bakje()
+        hoorntje_bakje()
 
 def bolletjes_stap1():
-    global vraag
+    global vraag, aantalbak
     vraag = int (input("Hoeveel bolletjes wilt u?: "))
     if vraag >=1 and vraag <=3:
-        hoorentje_bakje()
+        hoorntje_bakje()
     elif vraag >=4 and vraag <=8:
+        aantalbak+=1
         smaken()
         print("Dan krijgt u van mij een bakje met {} bolletjes".format(vraag))
+        bon()
         bestellen()
+        
     else:
         grote()
         bolletjes_stap1()
+
 def smaken():
     nummer = 0
     for i in range(int(vraag)):
@@ -69,6 +106,7 @@ def smaken():
         else:
             sorry()
             smaken()
+
 #(----------------------------------funcions above---------------------------------------------------)
 begin()
 bolletjes_stap1()
